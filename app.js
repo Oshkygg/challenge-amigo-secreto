231e1fc005c126c9d15385027ca9f80d2dee5909
Óscar Granados <oscargranados2319@gmail.com>
2025-02-17 23:26:57 -0600

    Trabajo del primer dia, se inicio con una addEventListener para escuchar los eventos del DOMContentLoaded, de igual forma se uso un arrow function para que el evento de las llaves solo ocurra una vez el HTML este listo

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+document.addEventListener('DOMContentLoaded', () => { //Aseguramos que el código se ejecute cuando el HTML esté cargado
+
+
+// Capturar elementos del HTML 
+const inputNomnreAmigo = document.querySelector('.input-name'); // Usando la clase 'input-name'
+const botonAnadir = document.querySelector('.button-add'); // Usando la clase 'button-add'
+const listaDeAmigosUL = document.querySelector('#listaAmigos'); // Usando el ID 'listaAmigos'
+
+// ... (Aquí irán las demás funciones y lógica que iremos agregando) ...
+
+});
